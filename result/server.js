1b1ebd1aad86d960158fbcb40b981d4d269bfb33
sriniks25 <64155887+sriniks25@users.noreply.github.com>
2020-05-01 23:20:23 +0530

    Update server.js

--- result/server.js
+++ result/server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     async = require('async'),
-    pg = require("pg"),
+    pool = new pg.Pool(),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
@@ -19,7 +19,7 @@ io.sockets.on('connection', function (socket) {
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
-    pg.connect('postgres://postgres_user:postgres_password@db/postgres', function(err, client, done) {
+    pool.connect('postgres://postgres_user:postgres_password@db/postgres', function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
